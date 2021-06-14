18d582218e72f55045b86f9ec6c536a9ac760a82
Alessio Coser <alessio.coser@xpeppers.com>
2021-06-14 18:32:46 +0200

    add a broken test

---
+++ src/test/kotlin/com/alessiocoser/GameOfLifeTest.kt
@@ -0,0 +1,12 @@
+package com.alessiocoser
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+
+class GameOfLifeTest {
+
+    @Test
+    fun fixme() {
+        assertFalse(true)
+    }
+}
